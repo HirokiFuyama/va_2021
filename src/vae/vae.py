11441b135ee51e07c68d51baeb6e7b08959dadcc
hiroki <whistle.khbems.18@gmail.com>
2020-12-19 22:39:22 +0900

    [update] add class of ImageTransgorm and Imgdataset

--- src/vae/vae.py
+++ src/vae/vae.py
@@ -1,6 +1,8 @@
 import torch
-import torch.utils.data
 from torch import nn
+from PIL import Image
+from torchvision import transforms
+import torch.utils.data as data
 from torch.nn import functional as F
 
 
@@ -34,3 +36,36 @@ class VAE(nn.Module):
         mu, logvar = self.encode(x.view(-1, self.input_dim))
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
+
+
+class ImageTransform:
+    """
+    This class is preprocessing image.
+    """
+    def __init__(self, mean, std):
+        self.data_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std)
+        ])
+
+    def __call__(self, img):
+        return self.data_transform(img)
+
+
+class ImgDataset(data.Dataset):
+
+    def __init__(self, file_list, transform):
+        self.file_list = file_list
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, index):
+        """
+        Convert image to Tensor data type.
+        """
+        img_path = self.file_list[index]
+        img = Image.open(img_path)
+        img_transformed = self.transform(img)
+        return img_transformed
