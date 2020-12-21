95c06f21f3f33830259996bac194b695facd85c9
hiroki <whistle.khbems.18@gmail.com>
2020-12-21 22:11:15 +0900

    [add] image_loader.py

--- src/vae/vae.py
+++ src/vae/vae.py
@@ -1,8 +1,5 @@
 import torch
 from torch import nn
-from PIL import Image
-from torchvision import transforms
-import torch.utils.data as data
 from torch.nn import functional as F
 
 
@@ -37,36 +34,3 @@ class VAE(nn.Module):
         z = self.reparameterize(mu, logvar)
         z = self.decode(z)
         return z,  mu, logvar
-
-
-class ImageTransform:
-    """
-    This class is preprocessing image.
-    """
-    def __init__(self, mean, std):
-        self.data_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean, std)
-        ])
-
-    def __call__(self, img):
-        return self.data_transform(img)
-
-
-class ImgDataset(data.Dataset):
-
-    def __init__(self, file_list, transform):
-        self.file_list = file_list
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.file_list)
-
-    def __getitem__(self, index):
-        """
-        Convert image to Tensor data type.
-        """
-        img_path = self.file_list[index]
-        img = Image.open(img_path)
-        img_transformed = self.transform(img)
-        return img_transformed
